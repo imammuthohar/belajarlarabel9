0d196749922af8988f6091482d4a4639d0bf4d69
Imam Mutohohar <immut07@gmail.com>
2022-11-04 09:57:48 +0700

    membuat Post

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return 'hellow world';
 });
+Route::get('/blog', function () {
+    return view ('blog');
+});
+
+Route::get('/about', function () {
+    return view ('about');
+});
+
